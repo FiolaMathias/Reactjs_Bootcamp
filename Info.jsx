1dd5b79085cbe517ca85da4cb6978b6cfa4c1809
FiolaMathias <86547268+FiolaMathias@users.noreply.github.com>
2021-06-27 12:09:58 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        {" "}
+        <h1>Javascript and React.js Bootcamp </h1>
+        <p>A basic Web Development bootcamp for beginners.</p>
+      </div>
+      <div className="note">
+        <h1>Python and Data Visualization</h1>
+        <p>
+          A basic bootcamp on The Foundation of Data Visualization for
+          beginners.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Tableau Fundamentals</h1>
+        <p>A basic bootcamp with hands-on project for beginners.</p>
+      </div>
+      <div className="note">
+        <h1>Industrial Training and Internship</h1>
+        <p>
+          Get trained on tech stack and get internship certificate on
+          completion.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
